e98229354631822fab178d6f5b930f7c72e3c7aa
Dipti Jain <deeptihnd000@gmail.com>
2024-06-05 21:02:24 +0530

    changed styling in home page

--- viteProject01/src/App.jsx
+++ viteProject01/src/App.jsx
@@ -1,5 +1,15 @@
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <h1>Hello World!!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+        commodi hic perferendis? Laudantium, quos. Rem, consectetur quasi vel
+        qui neque deleniti quae nemo accusantium numquam sint recusandae
+        deserunt explicabo dolorem.
+      </p>
+    </>
+  );
 }
 
 export default App;
