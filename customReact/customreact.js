94111ec00a54f18bfbc3cce36fa9111fdd9aa749
Dipti Jain <deeptihnd000@gmail.com>
2024-06-06 17:43:30 +0530

    changed customRender function

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,8 +1,11 @@
 function customRender(reactElement, mainContainer) {
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
-  domElement.setAttribute("href", reactElement.props.href);
-  domElement.setAttribute("target", reactElement.props.target);
+  //   domElement.setAttribute("href", reactElement.props.href);
+  //   domElement.setAttribute("target", reactElement.props.target);
+  for (const prop in reactElement.props) {
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
   mainContainer.appendChild(domElement);
 }
 const reactElement = {
