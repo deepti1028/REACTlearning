85840e017485d7e77c4a03ad50f9efcd1a906a94
Dipti Jain <deeptihnd000@gmail.com>
2024-06-09 22:11:33 +0530

    changed main.jsx

--- Project1/src/main.jsx
+++ Project1/src/main.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouteProvider, createBrowserRouter } from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-  },
-]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouteProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 // npm install -D tailwindcss postcss autoprefixer
