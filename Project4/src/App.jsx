5f2b35253c2eabbd1bf183e83706321041d330e6
Dipti Jain <deeptihnd000@gmail.com>
2024-06-10 15:52:01 +0530

    cretaed login and profile component

--- Project4/src/App.jsx
+++ Project4/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
       <div>hello</div>
-    </>
+    </UserContextProvider>
   );
 }
 
