d58eaf6e6c698f1b36269e9308d9df10fa566449
Dipti Jain <deeptihnd000@gmail.com>
2024-06-10 19:44:41 +0530

    implemented functionality for storing data in localstorage

--- Project6/src/App.jsx
+++ Project6/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TodoProvider } from "./context";
 import { preview } from "vite";
 function App() {
@@ -31,6 +31,17 @@ function App() {
       })
     );
   };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
     <TodoProvider
       value={{ todos, updateTodo, deleteTodo, addTodo, toggleComplete }}
