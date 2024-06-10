43d00e0b436eee01819961695d702774e974fa4e
Dipti Jain <deeptihnd000@gmail.com>
2024-06-10 15:41:08 +0530

    created context provier

--- Project4/src/context/UserContext.js
+++ Project4/src/context/UserContext.js
@@ -1,3 +1,5 @@
 import React from "react";
 const UserContext = React.createContext();
 export default UserContext;
+//now we will wrap all the components by this UserContext
+//so it will become provider i.e it will provide data
