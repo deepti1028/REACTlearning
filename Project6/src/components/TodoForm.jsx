c26d853a3febdbe8f1ad6998eb4ed17025637e4f
Dipti Jain <deeptihnd000@gmail.com>
2024-06-10 19:58:55 +0530

    completed todo form

--- Project6/src/components/TodoForm.jsx
+++ Project6/src/components/TodoForm.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodo } from "../context";
 
 function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+  const add = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo({ task: todo, completed: false });
+    setTodo("");
+  };
   return (
-    <form className="flex">
+    <form onSubmit={add} className="flex">
       <input
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
