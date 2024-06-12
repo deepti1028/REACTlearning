2d9e7e7329d68786908eb6a5d881cccaceb6f9b3
Dipti Jain <deeptihnd000@gmail.com>
2024-06-12 21:33:05 +0530

    created dispatcher for add todo

--- Project7/src/components/AddTodo.jsx
+++ Project7/src/components/AddTodo.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useDispacth } from "react-redux";
+import { addTodo } from "../features/todo/tosoSlice";
 function AddTodo() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispacth();
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput("");
+  };
   return (
     <form onSubmit={add} className="flex">
       <input
