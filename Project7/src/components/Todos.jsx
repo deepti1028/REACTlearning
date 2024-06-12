f47a4471372f285ea002998d73dff2e63e5f6925
Dipti Jain <deeptihnd000@gmail.com>
2024-06-12 23:02:58 +0530

    created todos page

--- Project7/src/components/Todos.jsx
+++ Project7/src/components/Todos.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/tosoSlice";
 function Todos() {
-  return <div></div>;
+  const todoList = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  return;
+  <>
+    <div>Todos:</div>;
+    {todoList.map((todo) => {
+      <li key={todo.id}>
+        {todo.text}
+        <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
+      </li>;
+    })}
+  </>;
 }
 
 export default Todos;
